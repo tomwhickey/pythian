283b33904d462ba80a2d7142bbf0496d63dd527e
tomwhickey <tom.w.hickey@gmail.com>
2016-09-06 21:06:34 -0500

    Update checkip.sh

--- checkip.sh
+++ checkip.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+#
 # Script: checkip.sh
 # Desc:	  Query checkip.dyndns.org using curl for current ip address, remove html tags, 
-#	  and append output to logfile in "<datetimestamp> | Current IP Address: <ip>" format
+#	        and append output to logfile in "<datetimestamp> | Current IP Address: <ip>" format
+#
 
 DNAME="checkip.dyndns.org"
 LOGFILE="checkip.log"
